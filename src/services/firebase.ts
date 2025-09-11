8b3110b7777b38323e87d0652d494eb02ac62bf7
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-11 18:42:24 +0000

    firebase.ts

---
+++ src/services/firebase.ts
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import dotenv from "dotenv";
+dotenv.config()
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+};
+
+//initialize firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+const storage = getStorage();
+const auth = getAuth();
+const analytics = getAnalytics();
+
+export { app, db, auth, storage, analytics };
