b525a08944355c3c54b7bf7cad5aa0d222f49963
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-11 17:53:10 +0000

    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { db } from './src/services/firebase';
 
 export default function App() {
+  //test firebase inintialization
+  React.useEffect(() => {
+    console.log("Firebase initialized: ", db ? 'Success' : 'Failed');
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Welcome to CampusFinder! </Text>
+      <Text style={styles.subtitle} > Lost and Found App</Text>
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f5f5',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#666',
+  }
+})
