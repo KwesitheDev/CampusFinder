11c3027af009f76e0338b0704a30733b605c3f03
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-11 20:19:43 +0000

    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,36 +1,6 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { db } from './src/services/firebase';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
-  //test firebase inintialization
-  React.useEffect(() => {
-    console.log("Firebase initialized: ", db ? 'Success' : 'Failed');
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to CampusFinder! </Text>
-      <Text style={styles.subtitle} > Lost and Found App</Text>
-
-    </View>
-  )
+  return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 18,
-    color: '#666',
-  }
-})
